5416d2edcc3073a4fd560b2788adac6a15a668e6
Matias Maravi <matias.maravi@utec.edu.pe>
2025-11-20 13:11:30 -0500

    initial commit

---
+++ vizproo/vizproo/_frontend.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Copyright (c) MATIUS.
+# Distributed under the terms of the Modified BSD License.
+
+"""
+Information about the frontend package of the widgets.
+"""
+
+module_name = "vizproo-js"
+module_version = "^0.1.0"
